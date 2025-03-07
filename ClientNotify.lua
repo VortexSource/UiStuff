6c6b972417b2dbe1d4e389ae219a8195accc2fcd
Vortex Auto Build <th15newok99@gmail.com>
2025-03-07 21:27:10 +0100

    Update ClientNotify.lua

--- ClientNotify.lua
+++ ClientNotify.lua
@@ -14,7 +14,7 @@ function Nofitication:Notify(nofdebug, middledebug, all)
     
     ambientShadow.Name = "ambientShadow"
     ambientShadow.Parent = GUI
-    ambientShadow.AnchorPoint = Vector2.new(1, 1.5)
+    ambientShadow.AnchorPoint = Vector2.new(3, 3)
     ambientShadow.BackgroundTransparency = 1.000
     ambientShadow.BorderSizePixel = 0
     ambientShadow.Position = UDim2.new(0.91525954, 0, 0.936809778, 0)
@@ -29,7 +29,7 @@ function Nofitication:Notify(nofdebug, middledebug, all)
     Window.Parent = ambientShadow
     Window.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
     Window.BorderSizePixel = 0
-    Window.Position = UDim2.new(0, 5, 0, 5)
+    Window.Position = UDim2.new(0, 3, 0, 3)
     Window.Size = UDim2.new(0, 230, 0, 80)
     Window.ZIndex = 2
     
